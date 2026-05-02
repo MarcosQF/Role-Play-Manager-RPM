01e20369b27f2028f4f332385be069dc006c2863
Marcos Queiroz <marcosamfq123@gmail.com>
2026-05-02 18:36:09 -0300

    create base fixtures for tests

---
+++ tests/conftest.py
@@ -0,0 +1,30 @@
+import pytest
+from fastapi.testclient import TestClient
+from sqlmodel import Session, SQLModel, create_engine
+from sqlmodel.pool import StaticPool
+
+from rpm.main import app
+from rpm.core.database import get_session
+
+
+@pytest.fixture(name="session")
+def session_fixture():
+    engine = create_engine(
+        "sqlite://", connect_args={"check_same_thread": False}, poolclass=StaticPool
+    )
+    SQLModel.metadata.create_all(engine)
+    with Session(engine) as session:
+        yield session
+    SQLModel.metadata.drop_all(engine)
+
+
+@pytest.fixture(name="client")
+def client_fixture(session: Session):
+    def get_session_override():
+        yield session
+
+    app.dependency_overrides[get_session] = get_session_override
+    client = TestClient(app)
+    yield client
+
+    app.dependency_overrides.clear()
